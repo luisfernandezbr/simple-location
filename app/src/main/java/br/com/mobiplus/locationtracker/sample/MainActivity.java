7cb2dfc41038fa1e5d5c8936e2e44beac59ad74a
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2017-11-04 13:54:36 -0200

    LocationService configured and started by Sample Activity. Needs to handle permissions

--- app/src/main/java/br/com/mobiplus/locationtracker/sample/MainActivity.java
+++ app/src/main/java/br/com/mobiplus/locationtracker/sample/MainActivity.java
@@ -1,13 +1,37 @@
 package br.com.mobiplus.locationtracker.sample;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.os.Parcelable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import br.com.mobiplus.locationtracker.LocationTrackerService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Location location = intent.getParcelableExtra(LocationTrackerService.EXTRA_LOCATION);
+                Log.d(TAG, "onReceive: " + location.toString());
+            }
+        }, new IntentFilter(LocationTrackerService.ACTION_ON_LOCATION_UPDATE));
+
+        startService(new Intent(this, LocationTrackerService.class));
+
     }
 }
